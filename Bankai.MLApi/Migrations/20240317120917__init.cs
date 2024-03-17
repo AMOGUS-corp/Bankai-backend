6e59695cfafb4cae7a132d57307ffe56ebc5caa2
H0tab <ksasha18@mail.ru>
2024-03-17 15:11:56 +0300

    Add Migrations

---
+++ Bankai.MLApi/Migrations/20240317120917__init.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bankai.MLApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class _init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Datasets",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    CompressedData = table.Column<byte[]>(type: "bytea", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datasets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(128)", maxLength: 128, nullable: false),
+                    Description = table.Column<string>(type: "character varying(512)", maxLength: 512, nullable: false),
+                    Enabled = table.Column<bool>(type: "boolean", nullable: false),
+                    Created = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Modified = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    TrainDuration = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Engine = table.Column<int>(type: "integer", nullable: false),
+                    State = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false),
+                    Algorithm = table.Column<int>(type: "integer", nullable: false),
+                    Prediction = table.Column<int>(type: "integer", nullable: false),
+                    Data = table.Column<byte[]>(type: "bytea", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DatasetModel",
+                columns: table => new
+                {
+                    DatasetsId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ModelsId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DatasetModel", x => new { x.DatasetsId, x.ModelsId });
+                    table.ForeignKey(
+                        name: "FK_DatasetModel_Datasets_DatasetsId",
+                        column: x => x.DatasetsId,
+                        principalTable: "Datasets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DatasetModel_Models_ModelsId",
+                        column: x => x.ModelsId,
+                        principalTable: "Models",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feature",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    IsTarget = table.Column<bool>(type: "boolean", nullable: false),
+                    ModelId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feature", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Feature_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FeatureImportanceMetric",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    ModelId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FeatureImportanceMetric", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FeatureImportanceMetric_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HyperParameter",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    ModelId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HyperParameter", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HyperParameter_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Metric",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false),
+                    ModelId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Metric", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Metric_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ParameterStatistics",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Mean = table.Column<double>(type: "double precision", nullable: false),
+                    StandardDeviation = table.Column<double>(type: "double precision", nullable: false),
+                    StandardError = table.Column<double>(type: "double precision", nullable: false),
+                    Count = table.Column<double>(type: "double precision", nullable: false),
+                    FeatureImportanceMetricId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParameterStatistics", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ParameterStatistics_FeatureImportanceMetric_FeatureImportan~",
+                        column: x => x.FeatureImportanceMetricId,
+                        principalTable: "FeatureImportanceMetric",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DatasetModel_ModelsId",
+                table: "DatasetModel",
+                column: "ModelsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Feature_ModelId",
+                table: "Feature",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FeatureImportanceMetric_ModelId",
+                table: "FeatureImportanceMetric",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HyperParameter_ModelId",
+                table: "HyperParameter",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Metric_ModelId",
+                table: "Metric",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Models_Name",
+                table: "Models",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ParameterStatistics_FeatureImportanceMetricId",
+                table: "ParameterStatistics",
+                column: "FeatureImportanceMetricId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DatasetModel");
+
+            migrationBuilder.DropTable(
+                name: "Feature");
+
+            migrationBuilder.DropTable(
+                name: "HyperParameter");
+
+            migrationBuilder.DropTable(
+                name: "Metric");
+
+            migrationBuilder.DropTable(
+                name: "ParameterStatistics");
+
+            migrationBuilder.DropTable(
+                name: "Datasets");
+
+            migrationBuilder.DropTable(
+                name: "FeatureImportanceMetric");
+
+            migrationBuilder.DropTable(
+                name: "Models");
+        }
+    }
+}
